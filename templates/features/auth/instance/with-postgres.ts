28dd4ad8904d22b02b5de3f773445b576d279bdc
nsmichelj <nunezsalmeronm@gmail.com>
2026-03-24 02:40:34 -0400

    implement base templates for authentication guards and database

---
+++ templates/features/auth/instance/with-postgres.ts
@@ -0,0 +1,19 @@
+import db from "@/lib/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+  plugins: [nextCookies()],
+});
